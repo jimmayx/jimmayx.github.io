1dd52be72f71b739bac329d780e9a67dc1194878
Jimmayx <joshsim1999@hotmail.com>
2025-02-05 20:10:04 +1300

    fix 12

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250205.200614';
+const cacheName = 'chirpy-20250205.200936';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
