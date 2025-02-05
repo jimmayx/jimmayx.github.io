2170f9d22baa577bf0ab4d560774271ccedeb98b
Jimmayx <joshsim1999@hotmail.com>
2025-02-05 20:06:32 +1300

    add chatgpt line

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250205.200152';
+const cacheName = 'chirpy-20250205.200614';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
